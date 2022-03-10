7b7064d9be01621c319eb67b853a92b4a6a9a1e2
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-03-10 18:18:34 +0900

    404페이지 추가, success var->let

---
+++ Project_advise_clothes/src/main/frontend/src/NotFound.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NotFound() {
+    return(
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: 65,
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+            }}
+        >
+            404 잘못된 페이지입니다.
+        </div>
+    );
+};
+
+export default NotFound;

417d1af45710141fcb730a865c10fe953029bb35
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-03-21 13:56:42 +0900

    nickname 변경

--- Project_advise_clothes/src/main/frontend/src/Mypage.js
+++ Project_advise_clothes/src/main/frontend/src/Mypage.js
@@ -7,7 +7,7 @@ import { Modal } from "react-bootstrap";
 
 function Mypage(props) {
     let co = { fontSize: "20px" };
-    const [cookies, setCookies, removeCookie] = useCookies(['info']);
+    const [cookies, setCookies, removeCookie] = useCookies(['info'],['nickC']);
     let [change, setChange] = useState(false);
     let [nickChange, setNickChange] = useState('');
     let [btn, setBtn] = useState(true);
@@ -21,36 +21,35 @@ function Mypage(props) {
             <div style={co}>Mypage<hr/></div>
             <div className="info">
                 아이디 : {cookies.info.account}
-                
+
                 {/* 닉네임 변경 */}
-                <p>닉네임 : {cookies.info.nickname} 
-                {change? <div>
-                    <input name="nick" type="text" value={nickChange} onChange={onNickChangeHandler} placeholder="변경하실 닉네임을 입력해주세요"/>
-                   
-                    <button onClick={()=> {
-                        const fetch = async() => {
-                            try {
-                            const chan = {
-                                nickname : nickChange
+                <p>닉네임 : {cookies.info.nickname}
+                    {change? <div>
+                        <input name="nick" type="text" value={nickChange} onChange={onNickChangeHandler} placeholder="변경하실 닉네임을 입력해주세요"/>
+
+                        <button onClick={()=> {
+                            const fetch = async() => {
+                                try {
+                                    const chan = {
+                                        nickname : nickChange
+                                    }
+                                    await axios.put(`http://localhost:8080/api/users/${cookies.info.account}`, chan);
+                                }
+                                catch (e) {
+                                    console.log(e);
+                                }
                             }
-                            await axios.put(`http://localhost:8080/api/users/${cookies.info.account}`, chan);
-                        }
-                    catch (e) {
-                      console.log(e);
-                    }
-                   }
-            fetch()
-            setChange(null);
-                    }}>저장</button></div> : null}
+                            fetch()
+                            setChange(null);
+                        }}>저장</button></div> : null}
 
-                {btn? <button onClick={()=>{
-                setChange(!nickChange);
-                setBtn(!btn);
+                    {btn? <button onClick={()=>{
+                        setChange(!nickChange);
 
-                }}>닉네임 변경</button>: null}</p>
-                
+                    }}>닉네임 변경</button>: null}</p>
+
                 <p>email : {cookies.info.email} <button>이메일 변경</button></p>
-                
+
                 <p>전화번호 : {cookies.info.phoneNumber} <button>전화번호 변경</button></p>
 
             </div>

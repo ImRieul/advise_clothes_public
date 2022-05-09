fbbca24aec40eb29b5b3ea8c09ba9f23c06e9dd9
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-05-09 17:55:03 +0900

    login_cookies

--- Project_advise_clothes/src/main/frontend/src/Login/isLogin.js
+++ Project_advise_clothes/src/main/frontend/src/Login/isLogin.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { Cookies, useCookies } from "react-cookie";
+
+// npm i js-cookie 모듈 설치
 
 function isLogin() {
-    return !!localStorage.getItem("account");
+    // return !!localStorage.getItem("account");
+    return !!Cookies.get('auth');
 };
 
 export default isLogin

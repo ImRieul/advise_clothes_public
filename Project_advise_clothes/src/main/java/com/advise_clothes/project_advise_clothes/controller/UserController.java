cd8b2c6feee7671d085570d490f49dd3bec540ca
mojja-im <82865792+ImRieul@users.noreply.github.com>
2022-05-11 02:07:48 +0900

    UserController CrossOrigin 어노테이션 추가

--- Project_advise_clothes/src/main/java/com/advise_clothes/project_advise_clothes/controller/UserController.java
+++ Project_advise_clothes/src/main/java/com/advise_clothes/project_advise_clothes/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "http://52.79.195.60:3000")
 public class UserController {
 
     private final UserService userService;
